b9afefca18c794c82ddce71298b62c93ea589ceb
kiazh <kiashadow2007@gmail.com>
2026-04-22 11:04:11 -0400

    new func

--- main.c
+++ main.c
@@ -203,6 +203,7 @@ b32 mat_relu(matrix *out, const matrix *in){
     if (out->rows != in->rows || out-> cols){
         return false;
     }
+
     u64 size = (u64)out->rows * out->cols;
     for (u64 i = 0; i < size; i++){
         out->data[i] = MAX(0, in->data[i]);
@@ -212,10 +213,39 @@ b32 mat_relu(matrix *out, const matrix *in){
 
 
 b32 mat_softmax(matrix *out, const matrix *in){
+    if (out->rows != in->rows || out-> cols){
+        return false;
+    }
 
+    u64 size = (u64)out->rows * out->cols;
+    f32 sum = 0.0f; 
+    for (u64 i = 0; i < size; i++){
+        out->data[i] = expf(in->data[i]);
+        sum += out->data[i];
+    }
+
+    mat_scale(out, 1.0f / sum);
+    
+    return true;
 }
 
 
-b32 mat_cross_entropy_loss(matrix *out, const matrix *p, const matrix *q);
+b32 mat_cross_entropy_loss(matrix *out, const matrix *p, const matrix *q){
+    if (p->rows != q->rows || p->cols != q->cols){
+        return false; 
+    }
+    if (out->rows != q->rows || out->cols != q->cols){
+        return false; 
+    }
+    u64 size = (u64)out->rows * out->cols;
+    for (u64 i = 0; i < size; i++) {
+        out->data[i] = p->data[i] == 0.0f ? 
+        0.0f : p->data[i] * -logf(q->data[i]);
+    }
+    
+    return true;
+
+}
+
 b32 mat_softmax_add_grad(matrix *out, const matrix *softmax_out);
 b32 mat_cross_entropy_loss_grad(matrix *out, const matrix *p, const matrix *q);
