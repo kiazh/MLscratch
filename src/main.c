d7be197ce57e226d682a6562077ffbceefb1c8f0
kiazh <kiashadow2007@gmail.com>
2026-04-23 00:27:05 -0400

    template

--- src/main.c
+++ src/main.c
@@ -91,6 +91,35 @@ typedef struct {
     model_program cost_prog; 
 } model_context;
 
+model_var* mv_create(
+    mem_arena* arena, model_context* model, 
+    u32 rows, u32 cols, u32 flags
+);
+model_var* mv_relu(
+    mem_arena* arena, model_context* model, 
+    model_var* input, u32* flags
+);
+model_var* mv_softmax(
+    mem_arena* arena, model_context* model, 
+    model_var* input, u32 flags
+);
+model_var* mv_add(
+    mem_arena* arena, model_context* model, 
+    model_var* a, model_var* b, u32 flags
+);
+model_var* mv_sub(
+    mem_arena* arena, model_context* model, 
+    model_var* a, model_var* b, u32 flags
+);
+model_var* mv_matmul(
+    mem_arena* arena, model_context* model, 
+    model_var* a, model_var* b, u32 flags
+);
+model_var* mv_cross_entropy(
+    mem_arena* arena, model_context* model, 
+    model_var* p, model_var* q, u32 flags
+);
+
 
 void draw_mnist_digit(f32* data);
 
