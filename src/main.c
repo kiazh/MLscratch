fb5c790d2848c4d1e3f504d8e808280dc67694ba
Kiarad <kiashadow2007@gmail.com>
2026-04-24 13:38:25 -0400

    org

--- src/main.c
+++ src/main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-#include "arena.h"
-#include "psgrdn.h"
-#include <math.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
+#include <math.h>
+
 #include "arena.c"
 #include "psgrdn.c"
+#include "arena.h"
+#include "psgrdn.h"
 #include "base.h"
+
 #define _CRT_SECURE_NO_WARNINGS
 
 typedef struct{
